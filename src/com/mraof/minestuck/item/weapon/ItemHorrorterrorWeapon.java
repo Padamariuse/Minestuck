1e0efec2e18094f80ff4f36f115dc6bd14299dd5
Padamariuse <noterrik@gmail.com>
2019-04-22 22:29:34 -0400

    Added a lil change to ItemHorrorterrorWeapon

--- src/com/mraof/minestuck/item/weapon/ItemHorrorterrorWeapon.java
+++ src/com/mraof/minestuck/item/weapon/ItemHorrorterrorWeapon.java
@@ -2,17 +2,20 @@ package com.mraof.minestuck.item.weapon;
 
 import java.util.Random;
 
+import com.mraof.minestuck.util.MinestuckSoundHandler;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.MobEffects;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
 
-public class ItemHorrorterrorWeapon extends ItemWeapon {
+public class  ItemHorrorterrorWeapon extends ItemWeapon {
 	
 	public ItemHorrorterrorWeapon(int maxUses, double damageVsEntity, double weaponSpeed, int enchantability, String name) {
 		super(maxUses, damageVsEntity, weaponSpeed, enchantability, name);
@@ -40,4 +43,12 @@ public class ItemHorrorterrorWeapon extends ItemWeapon {
         }
         return super.hitEntity(itemStack, target, attacker);
     }
+	
+	@Override
+	public void onUpdate(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
+		if(isSelected && itemRand.nextInt(1000) <= 1 && entityIn instanceof EntityPlayer) {
+			worldIn.playSound(entityIn.posX, entityIn.posY, entityIn.posZ, MinestuckSoundHandler.soundWhispers, SoundCategory.AMBIENT, 1, 1.0F, false);
+		}
+		super.onUpdate(stack, worldIn, entityIn, itemSlot, isSelected);
+	}
 }
